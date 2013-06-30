47d596db685e8d5971b482b2cd9ef608a680b315
Christopher Jackson <himself@cj-jackson.com>
2013-06-30 18:28:13 +0100

    Improvement to button!

--- button.go
+++ button.go
@@ -1,6 +1,35 @@
 package lispy
 
 func Button(li *Lispy) string {
-	const htmlstr = `{{$li := .}}<button{{if .Exist "value"}} value="{{.GetDel "value"|html}}"{{end}}{{if .Exist "name"}} name="{{.GetDel "name"|html}}"{{end}}{{if .Exist "type"}} type="{{.GetDel "type"|html}}"{{end}}{{if ExistRes "disabled"}} disabled{{end}}{{if ExistRes "autofocus"}} autofocus{{end}}{{if ExistRes "formnovalidate"}} formnovalidate{{end}}{{range .GetNames}} {{.|attr}}="{{$li.Get .|html}}"{{end}} />`
-	return li.HtmlRender(htmlstr)
+	str := `<button`
+
+	if li.Exist("value") {
+		str += ` value="` + li.GetDel("value") + `"`
+	}
+
+	if li.Exist("name") {
+		str += ` name="` + li.GetDel("name") + `"`
+	}
+
+	if li.Exist("type") {
+		str += ` type="` + li.GetDel("type") + `"`
+	}
+
+	if li.ExistRes("disabled") {
+		str += ` disabled`
+	}
+
+	if li.ExistRes("autofocus") {
+		str += ` autofocus`
+	}
+
+	if li.ExistRes("formnovalidate") {
+		str += ` formnovalidate`
+	}
+
+	str += li.GetParam()
+
+	str += `/>`
+
+	return str
 }
